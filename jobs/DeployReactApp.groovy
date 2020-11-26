c34de02065cd4b20e02de44dc3255c8a11dc7c32
akirakianna <kianna.akiralove@gmail.com>
2020-11-26 18:26:32 +0000

    adding notes

--- jobs/DeployReactApp.groovy
+++ jobs/DeployReactApp.groovy
@@ -1,11 +1,15 @@
+//! Define your pipeline - of type pipeline
 pipelineJob("Deploy-React-App"){
     description("Deploys a React web application to AWS")
+    //! How long you want to keep your logs for & how many - stops Jenkins WS from getting blocked up
     logRotator {
         daysToKeep(5)
         numToKeep(20)
     }
+    //! We don't want 2 builds happening at the same time - this stops project from building at same time
     concurrentBuild(allowConcurrentBuild = false)
     triggers {
+      //! Want Jenkins to check this repo every minute and if there are changes, run a build
         scm("* * * * *"){
             ignorePostCommitHooks(ignorePostCommitHooks = false)
         }
